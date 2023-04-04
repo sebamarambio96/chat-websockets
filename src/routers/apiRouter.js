ab1985e336e8f2bf2658a0dc2a6e583edfa8eea0
Sebastian Marambio <s.marambiosandoval@gmail.com>
2023-04-04 17:30:10 -0400

    deploy

--- src/routers/apiRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { productsRouter } from "./productsRouter.js"
-import { cartsRouter } from "./cartsRouter.js"
-
-export const apiRouter = Router()
-
-apiRouter.use('/products', productsRouter)
-apiRouter.use('/carts', cartsRouter)
